20ca32ae4b754740991b90bb1e1c30b6cd2d9153
chimeziesolomon <39730078+chimeziesolomon@users.noreply.github.com>
2018-06-27 18:04:17 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -5,6 +5,34 @@
 
 function makeGrid() {
 
-// Your code goes here!
-
+const rows = $('#heightGrid').val();
+    const cols = $('#widthGrid').val(); 
+    const table = $('#pixelCanvas');   
+    // Removing old table if any
+    $('#pixelCanvas').children().remove();
+    // Creating table rows
+    for (let i = 0; i < rows; i++) {
+        $('#pixelCanvas').append('<tr></tr>');
+    }
+    // Creating cells in all row
+    for (let j = 0; j < cols; j++) {
+            $('#pixelCanvas tr').append('<td></td>');
+        }
+         /*let j = 0;
+        while (j < cols) {
+    $('#containerGrid tr').append('<td></td>');
+    j++;
+}*/
+        table.on("click", "td", function()  {
+        var color= $("input[type='color']").val();
+        $(this).attr("bgcolor",color);
+    });
 }
+$("input[type='submit']").click(function(e) {
+    e.preventDefault();
+    makeGrid();
+});
+$('#reset').click(function() {
+  $('#pixelCanvas').children().remove();  
+
+});
